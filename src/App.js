cdbf0bbcff6404e7fa001cfb211b6c60256c1ea3
Jari Kovalainen <itse@jarikovalainen.com>
2022-04-20 11:11:01 +0300

    eka

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+/* function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +19,68 @@ function App() {
       </header>
     </div>
   );
-}
+} */
+/* You can put any valid JavaScript expression inside the curly braces in JSX. Komponentti ja toiminnallisuus koodataan nimettynä funktiona tai anonyymina aukikirjoitetuna funktiona.
+*/
+const App = () => (
+  <div className="App">
+    <h1> Laurea FullStack 2022 React sovellus!</h1>
+
+    <h2>It is {new Date().toLocaleTimeString()}.</h2>
+
+    <Hello />
+    <CustomHello
+      author="Tuomas Holopainen"
+      greeting="'Tere Moro Hei!'"
+      color="blue" />
+    <CustomHello
+      author="Floor Jansen"
+      greeting="'Goedemorgen!'"
+      color="green"
+    />
+    <Elementti />
+  </div>
+);
+
+/* Luodaan uusi Hello komponentti, jota kutsutaan edellä olevasta App komponentista */
+const Hello = () => (
+  <div>
+    <p>Hei Maailma!</p>
+  </div>
+);
+
+const CustomHello = (props) => {
+  return (
+    <div>
+      <p className={props.color}>
+        {props.greeting} (<strong> {props.author} </strong> )
+      </p>
+    </div>
+  );
+};
+
+const user = {
+  firstName: 'Night',
+  lastName: 'wish',
+  avatarUrl: 'https://global-uploads.webflow.com/5e5a76c9c9fe8458a78d93eb/61b101f9faea13371d108eb4_promo_group%20copysmall-p-500.png',
+  linkki: "https://www.nightwish.com/"
+};
+
+/* const elementti = (
+  <div>
+    <h2>{user.firstName}   {user.lastName}</h2>
+    <a href={user.linkki}><img src={user.avatarUrl} alt="Kuvalinkki rikki..." /></a>
+  </div>
+) */
+
+const Elementti = () => {
+  return (
+    <div>
+      <h2>{user.firstName}{user.lastName}</h2>
+      <a href={user.linkki}><img src={user.avatarUrl} alt="Kuvalinkki rikki..." /></a>
+    </div>
+  );
+};
+
 
 export default App;
